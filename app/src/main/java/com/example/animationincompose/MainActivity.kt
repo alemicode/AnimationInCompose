aa7504d1019cda73062dc049b511d125be662a7c
mohamadalemi <mohamad.alemicode@gmail.com>
2024-09-15 21:51:48 +0330

    feat: Add scale animation

--- app/src/main/java/com/example/animationincompose/MainActivity.kt
+++ app/src/main/java/com/example/animationincompose/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.animationincompose.ui.theme.AnimationInComposeTheme
@@ -16,13 +19,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val selectedItem = remember { mutableStateOf(false) }
             AnimationInComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ParentComponent(
+                        selected = selectedItem.value,
+                        subtitle = "This is what I wanted to know and you cannot change my mind"
+                    ) {
+                        selectedItem.value = !selectedItem.value
+                    }
                 }
             }
         }

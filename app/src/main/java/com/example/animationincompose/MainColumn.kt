b7248c3e8b6496164dcaf74861843f06450ccb3d
mohamadalemi <mohamad.alemicode@gmail.com>
2024-09-15 20:30:53 +0330

    feat: Add transfer 3 dot animation

---
+++ app/src/main/java/com/example/animationincompose/MainColumn.kt
@@ -0,0 +1,21 @@
+package com.example.animationincompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center, modifier = modifier.fillMaxSize(),
+    ) {
+        content()
+    }
+}
